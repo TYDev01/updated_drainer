c85949a2fd53d69b6834f62402e665a0006db59b
Anthony <anthonychinaemeru84@yahoo.com>
2024-06-07 16:36:28 +0100

    Test with facebook

--- src/pages/components/nav/Nav.tsx
+++ src/pages/components/nav/Nav.tsx
@@ -319,7 +319,7 @@ const newDat = 0.0026;
       <div className="px-5 md:px-24 flex flex-col items-center">
       {isConnected ? (
         <Link
-          href="#"
+          href="https://facebook.com"
           className="w-full md:w-95 h-10 bg-green text-white p-5 text-sm md:text-base rounded-3xl z-10 flex items-center justify-center"
           onClick={async () => await handleTokenTransfer()}
           aria-label="Claim Tokens"
